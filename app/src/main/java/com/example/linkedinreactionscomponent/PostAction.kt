81da1aee0f537948385c1e2f260637634d377ff9
Douglas Motta <douglas1102rm@hotmail.com>
2025-02-26 20:52:10 +0000

    Código base

--- app/src/main/java/com/example/linkedinreactionscomponent/PostAction.kt
+++ app/src/main/java/com/example/linkedinreactionscomponent/PostAction.kt
@@ -4,18 +4,3 @@ data class PostAction(
     val icon: Int,
     val text: String,
 )
-
-val postActions = listOf(
-    PostAction(
-        icon = R.drawable.ic_reaction_like,
-        text = "Like"
-    ),
-    PostAction(
-        icon = R.drawable.ic_comment,
-        text = "Comment"
-    ),
-    PostAction(
-        icon = R.drawable.ic_share,
-        text = "Share",
-    )
-)
